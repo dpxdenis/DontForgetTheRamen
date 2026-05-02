c3247c6c57a4807e74c1eeb5cf88f845c69a6f71
Denis <Denis@DESKTOP-3480ETQ>
2026-05-02 16:14:58 +0200

    primeng setup

--- dontforgettheramen.client/src/app/app-module.ts
+++ dontforgettheramen.client/src/app/app-module.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
@@ -15,6 +17,11 @@ import { App } from './app';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ],
   bootstrap: [App]
 })
